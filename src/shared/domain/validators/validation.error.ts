f31772c4be88fb8d628aeeda0f588670ab64257f
Vicente Nascimento <vicente_nascimento@sicredi.com.br>
2024-03-06 23:49:08 +0000

    feat: create entityValidationErr

--- src/shared/domain/validators/validation.error.ts
+++ src/shared/domain/validators/validation.error.ts
@@ -1,1 +1,13 @@
+import { FieldsErrors } from "./validator-fields-interface";
+
 export class ValidationError extends Error { }
+
+export class EntityValidationError extends Error {
+    constructor(public errors: FieldsErrors, message = 'Validation Error') {
+        super(message);
+    }
+
+    count() {
+        return Object.keys(this.errors).length;
+    }
+}
